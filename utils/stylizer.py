c72246a5a891a3ff7ec9338706deb8eb34d6d92f
swingxu <swingxu@foxmail.com>
2019-07-29 21:21:20 +0800

    seg & style trans

---
+++ utils/stylizer.py
@@ -0,0 +1,82 @@
+import argparse
+import os
+import torch
+import torch.nn as nn
+from PIL import Image
+from os.path import basename
+from os.path import splitext
+from torchvision import transforms
+from torchvision.utils import save_image
+import net
+
+class styleTrans():
+    def __init__(self,device,decoder_path=None,transform_path=None,vgg_path=None):
+        self.device = device
+        self.decoder = net.decoder
+        self.vgg = net.vgg
+        self.network=net.Net(self.vgg,self.decoder)
+        self.sa_module = self.network.sa_module
+
+
+        self.decoder.eval()
+        self.sa_module.eval()
+        self.vgg.eval()
+
+        self.decoder.load_state_dict(torch.load(decoder_path))
+        self.sa_module.load_state_dict(torch.load(transform_path))
+        self.vgg.load_state_dict(torch.load(vgg_path))
+
+        self.norm = nn.Sequential(*list(self.vgg.children())[:1])
+        self.enc_1 = nn.Sequential(*list(self.vgg.children())[:4])  # input -> relu1_1
+        self.enc_2 = nn.Sequential(*list(self.vgg.children())[4:11])  # relu1_1 -> relu2_1
+        self.enc_3 = nn.Sequential(*list(self.vgg.children())[11:18])  # relu2_1 -> relu3_1
+        self.enc_4 = nn.Sequential(*list(self.vgg.children())[18:31])  # relu3_1 -> relu4_1
+        self.enc_5 = nn.Sequential(*list(self.vgg.children())[31:44])  # relu4_1 -> relu5_1
+
+        self.norm.to(device)
+        self.enc_1.to(device)
+        self.enc_2.to(device)
+        self.enc_3.to(device)
+        self.enc_4.to(device)
+        self.enc_5.to(device)
+        self.sa_module.to(device)
+        self.decoder.to(device)
+        print('model init')
+
+    def feat_extractor(self, content, style):
+        Content4_1 = self.enc_4(self.enc_3(self.enc_2(self.enc_1(content))))
+        Content5_1 = self.enc_5(Content4_1)
+
+        Style4_1 = self.enc_4(self.enc_3(self.enc_2(self.enc_1(style))))
+        Style5_1 = self.enc_5(Style4_1)
+
+        content_f=[Content4_1,Content5_1]
+        style_f=[Style4_1,Style5_1]
+        return content_f, style_f
+    
+    def stansform(self, content,style,alpha=0.4,interpolation_weights=None):
+        assert (0.0 <= alpha <= 1.0)
+        content_f, style_f=self.feat_extractor(content, style)
+        Fccc = self.sa_module(content_f,content_f)
+
+        if interpolation_weights:
+            _, C, H, W = Fccc.size()
+            feat = torch.FloatTensor(1, C, H, W).zero_().to(self.device)
+            base_feat = self.sa_module(content_f, style_f)
+            for i, w in enumerate(interpolation_weights):
+                feat = feat + w * base_feat[i:i + 1]
+            Fccc=Fccc[0:1]
+        else:
+            feat = self.sa_module(content_f, style_f)
+        feat = feat * alpha + Fccc * (1 - alpha)
+        return self.decoder(feat)
+
+def test_transform(size, crop):
+    transform_list = []
+    if size != 0:
+        transform_list.append(transforms.Resize(size))
+    if crop:
+        transform_list.append(transforms.CenterCrop(size))
+    transform_list.append(transforms.ToTensor())
+    transform = transforms.Compose(transform_list)
+    return transform
---
+++ utils/test.py
@@ -0,0 +1,113 @@
+# System libs
+import os
+import argparse
+from distutils.version import LooseVersion
+# Numerical libs
+import numpy as np
+import torch
+import torch.nn as nn
+from scipy.io import loadmat
+import csv
+import sys
+#sys.path.append('../')
+from models import ModelBuilder, SegmentationModule
+from lib.nn import user_scattered_collate, async_copy_to
+from lib.utils import as_numpy
+import cv2
+from tqdm import tqdm
+from config import cfg
+import net
+from PIL import Image
+from os.path import basename
+from os.path import splitext
+from torchvision import transforms
+from torchvision.utils import save_image
+from torchvision.utils import make_grid
+import pandas as pd
+from PIL import Image,ImageFilter
+from stylizer import styleTrans,test_transform
+from genMask import calmask
+
+cfg.merge_from_file("./config/ade20k-resnet50dilated-ppm_deepsup.yaml")
+
+# const for style trans
+content="./imgs/li.jpg"
+style = "./imgs/brushstrokes.jpg"
+vgg_path='./pretrained/style_models/vgg_normalised.pth'
+decoder_path='./pretrained/style_models/decoder_iter_100000.pth'
+transform_path='./pretrained/style_models/sa_module_iter_100000.pth'
+
+# Additional options
+content_size=512
+style_size=512
+crop='store_true'
+save_ext='.jpg'
+output_path='./output'
+
+# Advanced options
+preserve_color='store_true'
+alpha=0.6
+style_interpolation_weights=''
+preserve_color=False
+do_interpolation = False
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+assert content
+assert style
+
+content_tf = test_transform(content_size,crop)
+style_tf = test_transform(style_size,crop)
+# preprocessing the image
+content = Image.open(content)
+style = Image.open(style)
+csize = content.size
+
+"""
+ssize = style.size
+box = (ssize[0]-csize[0],ssize[1]-csize[1],ssize[0],ssize[1])
+style = style.crop(box)
+"""
+
+_content = content_tf(content)
+_style = style_tf(style)
+
+_style = _style.to(device).unsqueeze(0)
+_content = _content.to(device).unsqueeze(0)
+transformer = styleTrans(device=device,vgg_path=vgg_path,
+                        transform_path=transform_path,
+                        decoder_path=decoder_path)
+
+with torch.no_grad():
+    content_trans = transformer.stansform(content=_content,style=_style,alpha=alpha)
+content_trans = content_trans.cpu()
+
+
+# cal mask
+cm = calmask(cfg,gpu=0)
+img = cv2.cvtColor(np.asarray(content),cv2.COLOR_RGB2BGR)  
+
+mask = cm.inference(img=img)
+_max = pd.value_counts(mask.flatten()).keys()[0]
+mask = np.where(mask == _max, 255, 0)
+mask = Image.fromarray(mask.astype(np.uint8)).convert('L')
+mask.save('./mask.png')
+
+grid = make_grid(content_trans, nrow=8, padding=2, pad_value=0,normalize=False, range=None, scale_each=False)
+output_ndarr = grid.mul_(255).add_(0.5).clamp_(0, 255).permute(1, 2, 0).to('cpu', torch.uint8).numpy()
+content_s = Image.fromarray(output_ndarr)
+content_s.save('./ss.png')
+
+bg = style
+locx = bg.size[0]-content_s.size[0]
+locy = bg.size[1]-content_s.size[1]
+
+bg.paste(content_s,box=(locx,locy),mask=mask)
+
+bg.save('./mask.png')
+
+
+
+
+
+
+
