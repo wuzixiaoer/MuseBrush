598c2e51300120f8243bd56437f970c7cdfaa94d
demonsrhythm <644863173@qq.com>
2019-08-13 07:30:03 +0000

    stable v3(changed loc rule)

--- test_c.py
+++ test_c.py
@@ -3,18 +3,19 @@ from PIL import Image
 from utils.transfer import style_transfer
 
 t = style_transfer()
-style_dict = {'style_src':'./utils/imgs/shouter.jpg', 'patch_src':'./utils/imgs/shouter_patch.jpg',
-         'loc': (50, 500), 'alpha':0.9, 'gl_ratio':0.1, 'hsize':300, 'bg':None}
-result = t.transfer(Image.open('./utils/imgs/brad_pitt.jpg'), style_dict)
+style_dict = {'style_src':'./utils/imgs/BertheMorisot.jpg', 'patch_src':'./utils/imgs/BertheMorisot_patch.jpg',
+         'loc': (480, 923), 'alpha':0.8, 'gl_ratio':0.1, 'hsize':846, 'bg':'./utils/imgs/BertheMorisot_bg.jpg'}
+result = t.transfer(Image.open('./utils/imgs/hjy.jpg'), style_dict)
 result.save('utils/result/result.png', quality=95)
-# coast.jpg 700,420 0.9 0.8 128
-# 4.jpg 1300, 600 0.9 0.8 256
-# Edouard 280, 450 0.9 0.8 750
-# Egon 400, 0 0.9 0.2 818
-# timg 445, 240 0.9 0.1 260 big head
-# sunday 900, 650 0.8 0.2 400
-# Miklmaid 1050, 1300 0.9 0.2 300
-# reaper 1150, 1000 0.9 0.6 400
-# chamber 200, 200 0.9, 0.6 400
-# woman 0,0 0.9 0.2 1132
-# shouter 80, 500 0.9 0.1 300
+# coast.jpg 800,508 0.9 0.2 150
+# Miklmaid 1150, 1577 1 0.9 300
+# reaper 1250, 1419 0.9 0.1 400
+# chamber 300, 525 0.8, 0.6 350
+# woman 512,716 0.9 0.2 832
+# shouter 150, 650 0.9 0.1 300
+# Landscape 400, 572, 0.9, 0.6, 250
+# Twodogs 500, 749 0.9 0.6 300 0
+# Arles 300, 650 0.9 0.7 300
+# sunday 1100 860 0.8 0.1 380
+# BertheMorisot 480, 923 0.8 0.1 846
+
